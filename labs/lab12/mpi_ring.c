493b625ce94a28d4990151028bf0409a83cd6bc6
tallack <tallack@nuc12.wopr.nic.uoregon.edu>
2014-05-13 14:38:31 -0700

    finished lab 12

--- labs/lab12/mpi_ring.c
+++ labs/lab12/mpi_ring.c
@@ -18,8 +18,10 @@ int main(int argc, char** argv) {
     char allText[255*tot];
     MPI_Status mpistat;
 
-    int sender = (rank-1+tot)%tot;
-    int recver = (rank+1)%tot;
+    int sender = (rank+1)%tot;
+    int recver = (rank-1+tot)%tot;
+
+
     
     if(rank==0) {
         sprintf(text,"Hello from rank %d",rank);
